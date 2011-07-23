c246258882bfb519478a49afcea198bb749ab62c
Josh Matthews <josh@joshmatthews.net>
2011-07-23 00:18:17 -0700

    Silence nodes when they're removed. Add extra oscillators.

--- audio.js
+++ audio.js
@@ -1,5 +1,8 @@
-var bufSize = 8192;
-var sampleRate = 44100.0;
+/* -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+var bufSize = /*1024*/8192;
+var sampleRate = 44100;
+var bufferTime = Math.floor(1000 / (sampleRate / bufSize));
 
 var output = new Audio();
 
@@ -7,10 +10,17 @@ if ( typeof output.mozSetup === 'function' ) {
   output.mozSetup(1, sampleRate, 1);
 }
 
+var silence = new Float32Array(bufSize);
+
 var audioWriter = function(s) {
+  output.mozWriteAudio([]);
   if (s != null)
     output.mozWriteAudio(s);
+  else
+    output.mozWriteAudio(silence);
 }
+
+//////////////////////////
 
 function mixInputs() {
   if (!this.inputs.length) {
@@ -26,6 +36,10 @@ function mixInputs() {
 
 var HomeNode = new Node(0, 0, Circle, mixInputs);
 
+nodeList.push(HomeNode);
+
+/////////////
+
 function generateAudio() {
   var homeNode = nodeList[0];
   audioWriter(homeNode.generateSignal.call(homeNode));
@@ -33,11 +47,43 @@ function generateAudio() {
 
 ///////////
 
-function makeOsc(x, y, freq, harmonic) {
+function makeSimpleOsc(x, y, type, freq, harmonic) {
   var osc = new Node(x, y, Square, function() {return this.signal; });
-  osc.dsp = new Oscillator(Oscillator.sine, freq*harmonic, 1/harmonic, bufSize, sampleRate);
+  osc.dsp = new Oscillator(type, freq*harmonic, 1/harmonic, bufSize, sampleRate);
   osc.signal = osc.dsp.generate();
   return osc;
 }
 
-nodeList.push(HomeNode);
+function makeSineOsc(x, y, freq, harmonic) {
+  return makeSinOsc(x, y, DSP.SINE, freq, harmonic);
+}
+
+function makeSawOsc(x, y, freq, harmonic) {
+  return makeSinOsc(x, y, DSP.SAW, freq, harmonic);
+}
+
+function makeSquareOsc(x, y, freq, harmonic) {
+  return makeSinOsc(x, y, DSP.SQUARE, freq, harmonic);
+}
+
+function makeTriangleOsc(x, y, freq, harmonic) {
+  return makeSinOsc(x, y, DSP.TRIANGLE, freq, harmonic);
+}
+
+///////////
+
+function makeSquareWave(x, y, freq) {
+  var sq = new Node(x, y, Square, function() {return this.signal;});
+  sq.dsp = new Oscillator(Oscillator.Sine, freq, 1, bufSize, sampleRate);
+  for (var i = 1; i <= 5; i++) {
+    var h = new Oscillator(Oscillator.Sine, freq*(i*2), 1/(i*2), bufSize, sampleRate);
+    h.generate();
+    sq.dsp.add(h);
+  }
+  sq.signal = sq.dsp.signal;
+  return sq;
+}
+
+//////////
+
+
